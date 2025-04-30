919a50abbe2f3ccb0d01313e0fe48af518a7cdf1
GrimRoge <serega_1702@mail.ru>
2025-04-30 16:09:56 +0300

    Hope last fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js';
+import runGame from '../src/index.js'
 
-const isEven = (num) => num % 2 === 0;
+const isEven = num => num % 2 === 0
 
 const generateRound = () => {
-    const number = Math.floor(Math.random() * 100) + 1;
-    const correctAnswer = isEven(number) ? "yes" : "no";
-    return [number.toString(), correctAnswer];
-};
+  const number = Math.floor(Math.random() * 100) + 1
+  const correctAnswer = isEven(number) ? 'yes' : 'no'
+  return [number.toString(), correctAnswer]
+}
 
-const rules = "Answer \"yes\" if the number is even, otherwise answer \"no\".";
-runGame(rules, generateRound);
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".'
+runGame(rules, generateRound)
--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 const runGame = (rules, generateRound) => {
-    console.log("Welcome to the Brain Games!");
-    const name = readlineSync.question("May I have your name? ");
-    console.log(`Hello, ${name}!\n${rules}`);
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!\n${rules}`)
 
-    let correctAnswers = 0;
-    const roundsToWin = 3;
+  let correctAnswers = 0
+  const roundsToWin = 3
 
-    while (correctAnswers < roundsToWin) {
-        const [question, correctAnswer] = generateRound();
-        console.log(`Question: ${question}`);
-        const userAnswer = readlineSync.question("Your answer: ").trim();
+  while (correctAnswers < roundsToWin) {
+    const [question, correctAnswer] = generateRound()
+    console.log(`Question: ${question}`)
+    const userAnswer = readlineSync.question('Your answer: ').trim()
 
-        if (userAnswer === correctAnswer) {
-            console.log("Correct!");
-            correctAnswers += 1;
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Let's try again, ${name}!`);
-            return;
-        }
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!')
+      correctAnswers += 1
     }
+    else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
+    }
+  }
 
-    console.log(`Congratulations, ${name}!`);
-};
+  console.log(`Congratulations, ${name}!`)
+}
 
-export default runGame;
+export default runGame
